ebcc488fb5b4c9e326e2a395561327bebf8b115f
Tapiiaa <ptapilob@myuax.com>
2024-03-03 21:25:44 +0100

    Ejercicio 2 terminado

--- src/Ejercicio2/Cliente.java
+++ src/Ejercicio2/Cliente.java
@@ -1,4 +1,94 @@
 package Ejercicio2;
-
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Cliente {
-}
+    static ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+    static Scanner scanner = new Scanner(System.in);
+    private String nombre;
+    private int id;
+
+    // Creamos el constructor
+    public Cliente(String nombre, int id) {
+        this.nombre = nombre;
+        this.id = id;
+    }
+
+    // Creamos los getters y setters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Creamos el método toString
+
+    public String toString() {
+        return "Cliente: " + nombre + " con id: " + id;
+    }
+
+    public static void crearCliente() {
+
+        System.out.println("Introduce el nombre del cliente: ");
+        String nombre = scanner.nextLine();
+        Cliente cliente = new Cliente(nombre, clientes.size() + 1);
+        System.out.println("Cliente creado:");
+        clientes.add(cliente);
+    }
+
+    public void opcionesMenu(int opcion) {
+        switch (opcion) {
+            case 1:
+                crearCliente();
+                menu();
+                break;
+            case 2:
+                mostrarClientes();
+                menu();
+                break;
+            case 3:
+                System.out.println("Saliendo del programa...");
+                break;
+            default:
+                System.out.println("Opción no válida");
+                menu();
+                break;
+        }
+    }
+
+        public void menu() {
+        int opcion;
+            System.out.println("Menu");
+            System.out.println("1. Crear cliente");
+            System.out.println("2. Mostrar clientes");
+            System.out.println("3. Salir");
+            System.out.println("Introduce una opción: ");
+            opcion = scanner.nextInt();
+            if(opcion < 1 || opcion > 3) {
+                System.out.println("Opción no válida");
+            }
+                opcionesMenu(opcion);
+        }
+        //Método para mostrar los clientes
+        public static void mostrarClientes() {
+            for (Cliente cliente : clientes) {
+                System.out.println(cliente);
+            }
+        }
+    }
+
+
+
+
+
+
--- src/Ejercicio2/Menu.java
+++ src/Ejercicio2/Menu.java
@@ -1,5 +1,42 @@
 package Ejercicio2;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Menu {
+    public static void main(String[] args) {
+        //Imprimimos el menú de la clase Cliente
+        Menu menu = new Menu();
+        //Imprimir menu
+        menu.menu();
+
+
+    }
+
+    public void menu() {
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+        System.out.println("Menu");
+        System.out.println("1. Crear cliente");
+        System.out.println("2. Mostrar clientes");
+        System.out.println("3. Salir");
+        System.out.println("Introduce una opción: ");
+        opcion = scanner.nextInt();
+        if(opcion < 1 || opcion > 3) {
+            System.out.println("Opción no válida");
+        }
+        if(opcion == 1){
+            Cliente.crearCliente();
+            menu();
+        }
+        if(opcion == 2){
+            Cliente.mostrarClientes();
+            menu();
+        }
+        if(opcion == 3){
+            System.out.println("Saliendo del programa...");
+        }
+
+    }
 
 }
